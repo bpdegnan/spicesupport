1a9b7601d7cc7f9ea1711f37652d035532107a89
Brian Degnan <bpdegnan@users.noreply.github.com>
2026-09-23 09:19:47 -0400

    added a EOL

--- setmeup.sh
+++ setmeup.sh
@@ -1,2 +1,3 @@
 
-export SPICE_LIB="$HOME/private/projects/semi/skywater-pdk/libraries/sky130_fd_pr/latest/models"
+export SPICE_LIB="$HOME/private/projects/semi/skywater-pdk/libraries/sky131_fd_pr/latest/models"
+
